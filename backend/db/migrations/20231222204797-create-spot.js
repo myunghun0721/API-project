a78769459fd590ee30624a178089c10625f8e05d
myunghun choi <myunghun0721@gmail.com>
2023-12-22 15:49:08 -0800

    fixing

--- backend/db/migrations/20231222204797-create-spot.js
+++ backend/db/migrations/20231222204797-create-spot.js
@@ -46,12 +46,13 @@ module.exports = {
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTMAP')
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       },
       updatedAt: {
         allowNull: false,
         type: Sequelize.DATE,
-        defaultValue: Sequelize.literal('CURRENT_TIMESTMAP')
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+
       }
     });
   },
