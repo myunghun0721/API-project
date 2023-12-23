f37eb9b7d957aab1911f80f08ddf288acc35b52c
myunghun choi <myunghun0721@gmail.com>
2023-12-22 16:00:27 -0800

    fixed migration

--- backend/db/migrations/20231222204798-create-booking.js
+++ backend/db/migrations/20231222204798-create-booking.js
@@ -1,4 +1,9 @@
 'use strict';
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
@@ -39,9 +44,10 @@ module.exports = {
         type: Sequelize.DATE,
         defaultValue: Sequelize.literal('CURRENT_TIMESTMAP')
       }
-    });
+    }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Bookings');
+    options.tableName = "Bookings";
+    return queryInterface.dropTable(options);
   }
 };
